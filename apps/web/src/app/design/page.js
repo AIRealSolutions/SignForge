7998c4302b7c1d2915108b52e5840a9e060a4a33
Marc Spencer <126417630+AIRealSolutions@users.noreply.github.com>
2026-07-21 20:50:05 -0400

    style(design): load phase one editor styles

--- apps/web/src/app/design/page.js
+++ apps/web/src/app/design/page.js
@@ -1,5 +1,7 @@
+import styles from "./DesignStudioClient.module.css";
 import DesignStudioClient from "./DesignStudioClient";
 
 export default function DesignStudioPage() {
+  void styles;
   return <DesignStudioClient />;
 }
